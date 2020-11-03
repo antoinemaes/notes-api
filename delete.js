6afd55aca2ce8a4b3b81594410a64b24efd94711
Antoine Maes <antoinemaes@gmail.com>
2020-11-03 19:16:21 +0100

    delete note api

---
+++ delete.js
@@ -0,0 +1,16 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async function(event) {
+
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+
+    await dynamoDb.delete(params);
+    return { status: true };
+});
