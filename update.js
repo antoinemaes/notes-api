2b5dd372ce00160b85b928f48a97e583306b46d1
Antoine Maes <antoinemaes@gmail.com>
2020-11-03 19:07:54 +0100

    update note api

---
+++ update.js
@@ -0,0 +1,24 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async function(event) {
+
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        },
+        UpdateExpression: "SET content = :content, attachment = :attachment",
+        ExpressionAttributeValues: {
+            ":content": data.content || null,
+            ":attachment": data.attachment || null,
+        },
+        ReturnValues: "ALL_NEW"
+    };
+
+    console.log(params);
+    await dynamoDb.update(params);
+    return { status: true };
+});
